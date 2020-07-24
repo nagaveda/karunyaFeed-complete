6cf31cc4fc8e0014022e0ba98a03382b71f60bf2
Nagaveda Sai Kumar Reddy <45199655+nagaveda@users.noreply.github.com>
2020-07-24 13:09:21 +0530

    Deleted Dev.js file in config

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://nagaveda:sFjvHBPIueyYLdUd@cluster0-kts6p.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_KEY: "cdscdfdvfasvsvsv",
-    SENDGRID_API: "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",
-    EMAIL: "http://localhost:3000"
-}
